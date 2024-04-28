ee6d27e9ff06c20f4cd7df718176dd1e9cc1ee25
DianaCtes <dimctes@gmail.com>
2024-04-27 19:20:20 -0500

    se crea función para calcular el area de un triangulo

---
+++ app.js
@@ -0,0 +1,7 @@
+//Función que calcula el area de un triangulo
+function calculateTriangleArea (base, height) {
+    var area = (base * height) / 2 
+    return area
+}
+var area1 = calculateTriangleArea (2, 4)
+console.log(area1)
